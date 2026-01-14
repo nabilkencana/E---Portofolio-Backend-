efdd30fc3eb4472f8f4f4a9f39d373f4d268322c
Nabilkencana22 <mohammad_kencana_33rpl@student.smktelkom-mlg.sch.id>
2026-01-14 16:52:05 +0700

    import cloudinary

--- src/portofolio/portofolio.module.ts
+++ src/portofolio/portofolio.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PortfolioService } from './portofolio.service';
 import { PortfolioController } from './portofolio.controller';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
+  imports : [CloudinaryModule],
   controllers: [PortfolioController],
   providers: [PortfolioService],
 })
